ca1192f7120f01a0f42d8a1bf0f972afed1fb034
rcyl <raymondchuayitlong@gmail.com>
2016-05-26 11:38:51 +0800

    Add rotating gear and victory conditions

--- js/myScript.js
+++ js/myScript.js
@@ -4,12 +4,16 @@ var main = function(){
 
     var grid, count, n, inPlay, com, pla;
 
+
+
     $("#btn-player").click(function(){
         inPlay = true;
         com = 'O';
         pla = 'X';
         init();
         $(".btn").hide();
+        rotate();
+        //turnMsg();
     })
 
     $("#btn-com").click(function(){
@@ -18,35 +22,36 @@ var main = function(){
         pla = 'O';
         init();
         $(".btn").hide();
-        place(initial());
+        rotate();
+        //place(initial());
     })
 
     $("td").click(function(){
-        if (inPlay){
-            $(this).unbind('mouseover', mouseOver).unbind('mouseout', mouseOut);
-            $(this).text(pla);
-            $(this).css("color", "rgba(0,0,0,1");
-
-            var id = this.id;            
-
+        var id = this.id;  
+        if (inPlay && grid[Math.floor(id/n)][id%n] == ''){
+            var cur = $(this);
+            fill(cur);
+            gearMsg();
             grid[Math.floor(id/n)][id%n] = pla;
             count++;
 
             if (count==n*n) {
-                $(".btn").show();
-                inPlay = false;
-            }
+                drawMsg();
+                endGame();
+            } 
 
             var pos = -1;
             if (count >2 ) pos = traverse(id);
             else pos = initial();
 
+            //gearMsg();
             place(pos);
+            turnMsg();
 
             if (count==n*n) {
-                $(".btn").show();
-                inPlay = false;
-            }
+                drawMsg();
+                endGame();
+            } 
         }
     })
 
@@ -55,8 +60,14 @@ var main = function(){
         $(this).text(pla);
     }
     function mouseOut(){
-        $(this).css("color", "rgba(0,0,0,1");
+        $(this).css("color", "black");
         $(this).text("");
+    }
+
+    var fill = function(cur){
+        cur.unbind('mouseover', mouseOver).unbind('mouseout', mouseOut);
+        cur.text(pla);
+        cur.css("color", "black");
     }
 
     var init = function(){
@@ -73,6 +84,8 @@ var main = function(){
         }
         $("td").text("");
         $("td").bind('mouseover', mouseOver).bind('mouseout', mouseOut);
+        $("td").css("color", "black");
+        //$("#stats").text("");
     }
 
     var initial = function(){
@@ -83,13 +96,21 @@ var main = function(){
         return (n-1)*n+(n-1);
     }
 
+    var points = function(countC, countP){
+
+        if (countC == 2 && countP == 0) return 6; //win
+        if (countC == 0 && countP == 2) return 5; //block
+        if (countC == 1 && countP == 0) return 2; //fork
+        //if (countC == 0 && countP == 1) return 1; //block fork
+        return 0; 
+    }
+
     var traverse = function(id){
         var max = -1, index = -1, points = 0;
         for(var i=0;i<n;i++){
             for(var j=0;j<n;j++){
                 if (grid[i][j] !== '') { continue; }
                 points = calc(i,j); 
-                if (points == 6) return i*n+j;
                 if (points > max) {
                     max = points;
                     index = i*n+j;
@@ -100,41 +121,107 @@ var main = function(){
     }
 
     var hor = function(i,j){
-        var countC = 0; countP = 0;
+        var countC = 0; countP = 0, p = 0;
         for(var k=0;k<n;k++) {
             if (grid[i][k]==com) countC++;
             else if (grid[i][k]==pla) countP++;
         }
-        return points(countC, countP);
+        p = points(countC, countP);
+        if (p==6){
+            for(var k=0;k<n;k++){
+                mark(i*n+k);
+            }
+            winMsg();
+            endGame();
+        }
+        return p;
     }
 
     var ver = function(i, j){
-        var countC = 0; countP = 0;
+        var countC = 0; countP = 0, p = 0;
         for(var k=0;k<n;k++) {
             if (grid[k][j]==com) countC++;
             else if (grid[k][j]==pla) countP++;
         }
-        return points(countC, countP); 
+        p = points(countC, countP); 
+        if (p == 6) {
+            for(var k=0;k<n;k++){
+                mark(k*n+j);
+            }
+            winMsg();
+            endGame();
+        }
+        return p;
     }
 
     var diag = function(i, j){
         if (i!=j) return 0;
-        var countC = 0; countP = 0; 
+        var countC = 0; countP = 0, p = 0;
         for(var k=0;k<n;k++) {
             if (grid[k][k]==com) countC++;
             else if (grid[k][k]==pla) countP++;
         }
-        return points(countC, countP);
+        p = points(countC, countP);
+        if (p== 6){
+            for(var k=0;k<n;k++){
+                mark(k*n+k);
+            }
+            winMsg();
+            endGame();
+        }
+        return p;
     }
 
     var rdiag = function(i, j){
         if (i+j!=n-1) return 0;
-        var countC = 0; countP = 0; 
+        var countC = 0; countP = 0, p = 0; 
         for(var k=0;k<n;k++) {
             if (grid[k][n-1-k]==com) countC++;
             else if (grid[k][n-1-k]==pla) countP++;
         }
-        return points(countC, countP);
+        p = points(countC, countP);
+        if (p== 6){
+            for(var k=0;k<n;k++){
+                mark(k*n+(n-1-k));
+            }
+            winMsg();
+            endGame();
+        }
+        return p;
+    }
+
+
+    var mark = function(id){
+        $("#" + id).css("color", "red");
+    }
+
+    var endGame = function(){
+        inPlay = false;
+        $(".btn").show();
+    }
+
+    var winMsg = function (){
+        $("#stats").html("Better luck next time.");
+    }
+
+    var drawMsg = function (){
+        $("#stats").html("Nobody wins.");
+    }
+
+    var turnMsg = function(){
+        $("#stats").html("Your turn, human.");
+    }
+
+    var rotate = function(){
+        var angle = 0;
+        var intervalID = setInterval(function() {
+            angle +=15;
+            if (angle==300) clearInterval(intervalID);
+            $("#gear").css("-webkit-transform", "rotate("+angle+"deg");
+            $("#gear").css("-moz-transform", "rotate("+angle+"deg");
+            $("#gear").css("-o-transform", "rotate("+angle+"deg");
+            $("#gear").css("transform", "rotate("+angle+"deg");
+        }, 50);
     }
 
     var calc = function(i,j){
@@ -147,22 +234,13 @@ var main = function(){
 
         return points;
     }
-
-    var points = function(countC, countP){
-
-        if (countC == 2 && countP == 0) return 6; //win
-        if (countC == 0 && countP == 2) return 5; //block
-        if (countC == 1 && countP == 0) return 2; //fork
-        //if (countC == 0 && countP == 1) return 1; //block fork
-        return 0; 
-    }
-
+    
     var place = function(pos){
         var cur = $("#" + pos);
         grid[Math.floor(pos/n)][pos%n] = com;
-        count++;
         cur.text(com);
         cur.unbind('mouseover', mouseOver).unbind('mouseout', mouseOut);
+        count++;
     }
 
 }
